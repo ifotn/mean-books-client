1c281f29fbdfcb9ee70d8a5591354be1201150eb
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-02-18 16:46:56 -0500

    l7/2 - mock book data in form

--- src/app/components/books/books.ts
+++ src/app/components/books/books.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+// define Book class that will be used by view
+export class Book {
+  _id: string | undefined;
+  title: string | undefined;
+  year: number | undefined;
+}
 
 @Component({
   selector: 'app-books',
-  imports: [],
-  templateUrl: './books.html',
-  styleUrl: './books.css',
+  imports: [FormsModule],
+  templateUrl: './books.html'
 })
 export class Books {
+  // create in-memory array of books
+  BOOKS: Book[] = [
+    { _id: 'abc123', title: 'Adrift', year: 2023 },
+    { _id: 'def456', title: 'A Long Way Down', year: 2006 },
+    { _id: 'ghi789', title: 'High Fidelity', year: 1995 }
+  ];
 
+  // component to manage single book
+  book: Book | undefined;
+
+  // select book when an item in list is clicked
+  onSelect(selectedBook: Book): void {
+    this.book = selectedBook;
+  }
+
+  clearForm(): void {
+    this.book = undefined;
+  }
 }
