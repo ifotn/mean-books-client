9262cc7a6e80ef9ae236a228e04c4ddd6fd7dd34
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-02-18 15:55:39 -0500

    l7/1 - angular setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('mean-books-client');
+  // any properties set in this component are accessible in the html view
+  protected readonly title: string = 'Welcome to our Library';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Books } from './app/components/books/books';
 
-bootstrapApplication(App, appConfig)
+// first param: default component to load when app starts
+bootstrapApplication(Books, appConfig)
   .catch((err) => console.error(err));
