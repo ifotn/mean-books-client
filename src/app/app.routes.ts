4f0fb0bbda57516d5faf848b97feb3b77f36ac56
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-03-11 15:50:33 -0400

    l9/1 - first angular routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Books } from './components/books/books';
+import { Register } from './components/register/register';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+// routes array lists all the url mapping to corresponding components
+// paths do NOT include leading slash /
+export const routes: Routes = [
+    { path: 'books', title: 'Books', component: Books },
+    { path: 'register', title: 'Register', component: Register },
+    { path: 'login', title: 'Login', component: Login }
+];
