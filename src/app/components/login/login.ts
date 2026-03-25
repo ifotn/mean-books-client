e997548bdccaddbd1df9f0eab37b3ea403588ae5
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-03-25 16:25:07 -0400

    l11/2 - login, protected post/put/delete calls

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,44 @@
+import { AuthService } from '../../services/auth-service';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  username: string | undefined;
+  password: string | undefined;
+  apiResponse: any;
+  message: string = 'Please enter your credentials'; // default message above form
+  messageClass: string = 'alert alert-info';  // blue bg default
 
+  // constructor w/dependencies
+  constructor(private authService: AuthService, private cdr: ChangeDetectorRef) {}
+
+  login() {
+    // create user object then pass to auth service to send to server api
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    return this.authService.login(user).subscribe({
+      next: response => {
+        this.apiResponse = response;
+        console.log(response);
+        this.message = 'Login Successful';
+        this.messageClass = 'alert alert-success';  // change message bg => green
+        this.cdr.detectChanges();
+      },
+      error: error => {
+        this.message = 'Invalid Login';
+        this.messageClass = 'alert alert-danger';  // change message bg => red
+        this.cdr.detectChanges();
+      }
+    });
+  }
 }
--- src/app/services/book-service.ts
+++ src/app/services/book-service.ts
@@ -16,23 +16,23 @@ export class BookService {
   // constructor: startup code of service.  Initiatlize HttpClient dependency so all methods can use
   constructor(private http: HttpClient) { }
 
-  // get all
+  // get all, public => no cookie w/jwt required
   getBooks(): Observable<Book[]> {
     return this.http.get<Book[]>(`${this.serverUrl}/api/v1/books`);
   }
 
-  // post
+  // post, private => pass cookie w/jwt in header
   createBook(book: Book): Observable<Book> {
-    return this.http.post<Book>(`${this.serverUrl}/api/v1/books`, book);
+    return this.http.post<Book>(`${this.serverUrl}/api/v1/books`, book, { withCredentials: true });
   }
 
-  // delete
+  // delete, private => pass cookie w/jwt in header
   deleteBook(_id: string): Observable<void> {
-    return this.http.delete<void>(`${this.serverUrl}/api/v1/books/${_id}`);
+    return this.http.delete<void>(`${this.serverUrl}/api/v1/books/${_id}`, { withCredentials: true });
   }
 
-   // put
+   // put, private => pass cookie w/jwt in header
   updateBook(book: Book): Observable<Book> {
-    return this.http.put<Book>(`${this.serverUrl}/api/v1/books/${book._id}`, book);
+    return this.http.put<Book>(`${this.serverUrl}/api/v1/books/${book._id}`, book, { withCredentials: true });
   }
 }
